e32c97937699c1dbc19b048758cbfb77bca81d10
elior <elior1001@gmail.com>
2022-12-20 10:26:28 +0200

    fat docekr file with entry point created

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
-FROM maven:3.5.2-jdk-8
+FROM maven:3.8.6-openjdk-8
+
+
 
 # copy source files
 COPY src /app/src
-COPY pom.xml /app
+COPY pom.xml LICENSE-GPLv3.txt /app/
 
+WORKDIR /app
 # build and verify
-RUN mvn -f /app/pom.xml clean verify
+RUN mvn verify
 
-# set the entrypoint script
-# COPY entrypoint.sh /entrypoint.sh
+# # set the entrypoint script
+COPY entrypoint.sh /entrypoint.sh
 
-# ENTRYPOINT ["/entrypoint.sh"]
+RUN ["chmod", "+x", "/entrypoint.sh"]
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /app/target
+export DB_DIALECT=HSQLDB
+export DB_URL=jdbc:hsqldb:file:lavagna
+export DB_USER=sa
+export DB_PASS=""
+export SPRING_PROFILE=dev
+
+unzip lavagna-1.1.3-SNAPSHOT-distribution.zip
+
+java -Xms64m -Xmx128m -Ddatasource.dialect="${DB_DIALECT}" \
+-Ddatasource.url="${DB_URL}" \
+-Ddatasource.username="${DB_USER}" \
+-Ddatasource.password="${DB_PASS}" \
+-Dspring.profiles.active="${SPRING_PROFILE}" \
+-jar ./lavagna-1.1.3-SNAPSHOT/lavagna/lavagna-jetty-console.war --headless
